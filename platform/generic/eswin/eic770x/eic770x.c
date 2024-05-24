02755439214b574df59a2797bd3187b2219e122b
Xiang W <wxjstz@126.com>
2024-05-24 21:43:03 +0800

    platform: eswin/eic770x: add tlbr_flush_limit
    Set flush limt to zero. flush all tlb always.
    
    Signed-off-by: Xiang W <wxjstz@126.com>

--- platform/generic/eswin/eic770x/eic770x.c
+++ platform/generic/eswin/eic770x/eic770x.c
@@ -1,6 +1,14 @@
 #include <platform_override.h>
 #include <sbi_utils/fdt/fdt_helper.h>
 
+/* Full tlb flush always */
+#define EIC770X_TLB_RANGE_FLUSH_LIMIT	0
+
+static u64 eic770x_get_tlbr_flush_limit(const struct fdt_match *match)
+{
+	return EIC770X_TLB_RANGE_FLUSH_LIMIT;
+}
+
 static const struct fdt_match eic770x_match[] = {
 	{ .compatible = "SiFive,FU800-dev" },
 	{ .compatible = "fu800-dev" },
@@ -10,4 +18,5 @@ static const struct fdt_match eic770x_match[] = {
 
 const struct platform_override eic770x = {
 	.match_table		= eic770x_match,
+	.tlbr_flush_limit	= eic770x_get_tlbr_flush_limit,
 };
